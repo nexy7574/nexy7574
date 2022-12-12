ff4233bf729dc46ad49fcde93c475c27d5d0abdd
eek7574 <me@nexy7574.cyou>
2022-12-12 11:18:11 +0000

    Prevent installer from automatically running zsh

--- dotfiles/install.bash
+++ dotfiles/install.bash
@@ -5,6 +5,7 @@ wget -V > /dev/null 2> /dev/null || sudo apt install wget || sudo pacman -S wget
 echo 'Downloading zsh config'
 wget https://github.com/EEKIM10/EEKIM10/raw/master/dotfiles/.zshrc -O ~/.zshrc || exit 2
 echo 'Installing oh-my-zsh'
+export RUNZSH=no
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || exit 3
 echo 'Installing zsh-autosuggestions'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit 4
